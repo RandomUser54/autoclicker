ce1e7bb23cf59f493d94aa23345024d5f3aa47a0
RandomUser54 <78670909+RandomUser54@users.noreply.github.com>
2021-03-28 14:35:15 -0400

    Create Window.h

---
+++ src/Window.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Autoclicker.h"
+#include "resource.h"
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+WNDCLASS wc;
+HWND hwnd;
+HWND button;
+HWND button1;
+HWND button2;
+HWND button3;
+HWND button4;
+HWND txtbox;
+HWND txtbox1;
+MSG msg;
+HIMAGELIST imagelist;
+HBITMAP img;
