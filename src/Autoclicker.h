0f40d414768f81d04475d8d2492599f734301bfc
RandomUser54 <78670909+RandomUser54@users.noreply.github.com>
2021-03-28 14:34:03 -0400

    Update and rename Header.h to Autoclicker.h

--- src/Autoclicker.h
+++ src/Autoclicker.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <Windows.h>
 #include <iostream>
+#include <sstream>
+#include <CommCtrl.h>
+#include "resource.h"
 using namespace std;
 extern int timesleep;
 extern HANDLE thread;
+extern int code;
 void consistent();
 void fast();
 void stop();
@@ -11,4 +15,3 @@ extern INPUT on[1];
 extern INPUT off[1];
 extern MOUSEINPUT clickon;
 extern MOUSEINPUT clickoff;
-extern int code;
