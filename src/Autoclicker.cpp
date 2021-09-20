3dc43092e1efc93863a0c5618f79604f870fd84b
RandomUser54 <78670909+RandomUser54@users.noreply.github.com>
2021-09-19 23:48:52 -0400

    Update Autoclicker.cpp

--- src/Autoclicker.cpp
+++ src/Autoclicker.cpp
@@ -3,28 +3,41 @@ INPUT on[1];
 INPUT off[1];
 MOUSEINPUT clickon;
 MOUSEINPUT clickoff;
+LARGE_INTEGER frequency;
+LARGE_INTEGER old;
+LARGE_INTEGER n;
+int timewait;
+void wait(float time) {
+	timewait = frequency.QuadPart * (time/1000);
+	QueryPerformanceCounter(&old);
+	while (n.QuadPart - old.QuadPart < timewait) {
+		QueryPerformanceCounter(&n);
+	}
+}
 void consistent() {
 	while (true) {
-		if (GetKeyState(code) >> 15) {
+		while (GetKeyState(code) < 0) {
 			SendInput(1, on, sizeof(on[0]));
-			Sleep(timesleep);
+			wait(timesleep);
 			SendInput(1, off, sizeof(off[0]));
-			Sleep(timesleep);
+			wait(timesleep);
 		}
+		Sleep(timesleep);
 	}
 }
 void fast() {
 	while (true) {
-		if (GetKeyState(code) >> 15) {
+		while (GetKeyState(code) < 0) {
 			SendInput(1, on, sizeof(on[0]));
-			Sleep(timesleep);
+			wait(timesleep);
 			SendInput(1, off, sizeof(off[0]));
 		}
+		Sleep(timesleep);
 	}
 }
 void stop() {
 	if (!TerminateThread(thread, 0)) {
-		cout << "Error terminating thread" << endl;
+		MessageBoxA(NULL, "Error terminating thread", "Thread Termination", MB_OK);
 	}
 	thread = NULL;
 }
