80a05bf4f454e9d35b5ed24fcfc64ad5b862683c
RandomUser54 <78670909+RandomUser54@users.noreply.github.com>
2021-09-19 23:51:00 -0400

    Update Autoclicker.h

--- src/Autoclicker.h
+++ src/Autoclicker.h
@@ -1,9 +1,5 @@
 #pragma once
 #include <Windows.h>
-#include <iostream>
-#include <sstream>
-#include <CommCtrl.h>
-#include "resource.h"
 using namespace std;
 extern int timesleep;
 extern HANDLE thread;
@@ -15,3 +11,4 @@ extern INPUT on[1];
 extern INPUT off[1];
 extern MOUSEINPUT clickon;
 extern MOUSEINPUT clickoff;
+extern LARGE_INTEGER frequency;
